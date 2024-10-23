984ecc43145d7e2c9f40a7a905f67b929484d3a6
Sarcasticsalad <samrajyabasnyat64@gmail.com>
2024-10-23 14:29:32 +0545

    Removed embedded repository virtual_assistant_Luna

---
+++ std_audio_name.py
@@ -0,0 +1,37 @@
+import os
+
+# Source Directory
+source_path = "./Samrajya_Audio"
+
+# Target Directory
+target_path = "./data/audio"
+
+def Rename_Files():
+    # Index to track file number
+    index = 26
+
+    # Iteration through each file in the given directory
+    for filename in os.listdir(source_path):
+        
+        # Constructing the old file path 
+        # This is necessary because to perform any operations on the file (like renaming it, moving it, or reading from it)
+        # you need the complete path to the file.
+        # os.listdir(old_path) only gives the file name and not the path
+
+        old_file_path = os.path.join(source_path, filename)
+
+        # Renaming the files
+        new_file_name = f"audio_sample_{index}.wav"
+
+        # Increasing the index
+        index += 1
+
+        # Constructing the new file path
+        new_file_path = os.path.join(target_path, new_file_name)
+
+        # Moving and Renaming the file
+        os.rename(old_file_path, new_file_path)
+
+
+Rename_Files()
+
