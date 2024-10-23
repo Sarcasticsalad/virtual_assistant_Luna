00326fe7eb26eba397408d29d42425125ba79111
Sarcasticsalad <samrajyabasnyat64@gmail.com>
2024-10-23 22:53:50 +0545

    Updated the path in the script

--- virtual_assistant_Luna/generate_dataset.py
+++ virtual_assistant_Luna/generate_dataset.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 # Define Directories
 audio = './data/audio'
-transcript = './data/transcripts'
+transcript = './data/transcript'
 
 # Get list of audio and transcript files
-audio_files =  sorted([f for f in os.listdir(audio) if f.endswith('wav')])
-transcript_files = sorted([f for f in os.listdir(transcript) if f.endswith('txt')])
+audio_files =  sorted([file for file in os.listdir(audio) if file.endswith('wav')])
+transcript_files = sorted([file for file in os.listdir(transcript) if file.endswith('txt')])
 
 # Ensure each list has same no of files
 if len(audio_files) != len(transcript_files):
@@ -15,8 +15,8 @@ if len(audio_files) != len(transcript_files):
 
 # Creating a Dictionary which consists of two keys that holds the location for all files
 data =  {
-           "audio_file" : [os.path.join(audio, f).replace('\\', '/') for f in audio_files],
-           "transcript_file" : [os.path.join(transcript, f).replace('\\', '/') for f in transcript_files] 
+           "audio_file" : [os.path.join(audio, file).replace('\\', '/') for file in audio_files],
+           "transcript_file" : [os.path.join(transcript, file).replace('\\', '/') for file in transcript_files] 
         }
 
 # Creating the DataFrame
